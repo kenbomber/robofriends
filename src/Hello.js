4dc081239f555633a7ac321e70cbf035e7a84c46
Ken_Bomber <kenbobonging@gmail.com>
2021-03-03 14:48:29 -0330

    example

--- src/Hello.js
+++ src/Hello.js
@@ -1,20 +1,34 @@
-import React, {Component} from "react";
+// import React, {Component} from "react";
 import './Hello.css'
 
-class Hello extends Component {
-    render() {
-        return(
-            <div className="firstComponent">
-                <h1 className="firstHeader">Hello World</h1>
-                <p>Welcome to React</p>
-                <ul className="firstList">
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                </ul>
-            </div>
-        );
-    };
-};
+// class Hello extends Component {
+//     render() {
+//         return(
+//             <div className="f2 tc">
+//                 <h1 className="firstHeader">Hello World</h1>
+//                 <p>{this.props.greeting}</p>
+//                 <ul className="firstList">
+//                     <li>Item 1</li>
+//                     <li>Item 2</li>
+//                     <li>Item 3</li>
+//                 </ul>
+//             </div>
+//         );
+//     };
+// };
+
+const Hello = (props) => {
+    return(
+        <div className="f2 tc">
+            <h1 className="firstHeader">Hello World</h1>
+            <p>{props.greeting}</p>
+            <ul className="firstList">
+                <li>Item 1</li>
+                <li>{props.dog}</li>
+                <li>Item 3</li>
+            </ul>
+        </div>
+);
+}
 
 export default Hello;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import './index.css';
 // import App from './App';
 import Hello from './Hello.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'
 
 ReactDOM.render(
-  <Hello />,
+  <Hello greeting={"Hello " + "React Ninja"} dog={"Dog"}/>,
   document.getElementById('root')
 );
-
+// greeting here is a property of the Hello component
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
